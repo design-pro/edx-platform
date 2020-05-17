20b67d71e2ed169482669f08afc8075432105fdd
Mohammad Reza Chavoshipour <mohamamd.chavoshipor@gmail.com>
2020-05-17 17:08:46 +0430

    fix compile language and add css

--- conf/locale/convert.sh
+++ conf/locale/convert.sh
@@ -1,2 +1,2 @@
 # for file in ./edx-platform/locale/*/*/*.po; do msgfmt $file -o $(dirname file)"/"$(basename ${file%.*}".mo"); done;
-for file in ./edx-platform/locale/*/*/*.po; do msgfmt $file -o $(dirname $file)/$(basename ${file%.*}).mo; done;
+for file in ./fa/*/*.po; do msgfmt $file -o $(dirname $file)/$(basename ${file%.*}).mo; done;
