4dd161d3d3a797f1dee8c54df8d796a4549fe25d
Mohammad Reza Chavoshipour <mohamamd.chavoshipor@gmail.com>
2020-05-22 14:58:58 +0430

    False debug settings

--- lms/envs/server.py
+++ lms/envs/server.py
@@ -2,3 +2,5 @@ from .devstack import *  # pylint: disable=wildcard-import, unused-wildcard-impo
 
 WEBPACK_CONFIG_PATH = 'webpack.prod.config.js'
 REQUIRE_DEBUG = DEBUG
+DEBUG = False
+DEFAULT_TEMPLATE_ENGINE['OPTIONS']['debug'] = False
